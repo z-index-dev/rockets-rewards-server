d2729451c82cf6ede7d52488023953427caa8012
Zach Shelton <zachshelton91@gmail.com>
2021-06-01 21:15:51 -0400

    Added async await to get request

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ app.get('/submit', (req, res) => {
 });
 
 // NB - Mongo will export file with the id
-app.get('/api/:id', (req, res) => {
+app.get('/api/:id', async (req, res) => {
   const user = User.findOne({ _id: req.params.id });
-  res.json({ user });
+  await res.json({ user });
 });
 
 app.listen(port);
