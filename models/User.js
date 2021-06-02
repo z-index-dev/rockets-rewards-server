831196bd39855295f309945f428ff2479df206ea
Zach Shelton <zachshelton91@gmail.com>
2021-06-01 20:43:07 -0400

    Fixing User.js file names, require lines

--- models/User.js
+++ models/User.js
@@ -62,6 +62,6 @@ const UserSchema = new Schema({
   }
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Schema variables
-const User = require('/models/user');
+const User = mongoose.model('User');
 
 app.use(bodyParser.json());
 app.use(cors());
