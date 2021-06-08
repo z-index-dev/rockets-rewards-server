dbbd0fa3801b2193a9ffb87fb94f6b583e523eff
Zach Shelton <zachshelton91@gmail.com>
2021-06-08 11:20:42 -0500

    Trying revamped replaceOne method

--- server.js
+++ server.js
@@ -26,16 +26,41 @@ const db = mongoose.connection;
 db.on('error', () => console.log('Error connecting to db'));
 db.once('open', () => console.log('Connected to db'));
 
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
   const submission = new Submission(req.body);
   const requests = db.collection('requests');
 
-  requests.insertOne(submission, (err, collection) => {
-    if(err) {
-      throw err;
-    }
-    console.log('Record inserted');
-  });
+  const query = { uuid: submission.uuid };
+  const options = { upsert: true };
+  const replacement = {
+    product_01: submission.product_01,
+    product_02: submission.product_02,
+    product_03: submission.product_03,
+    product_04: submission.product_04,
+    product_05: submission.product_05,
+    product_06: submission.product_06,
+    product_07: submission.product_07,
+    product_08: submission.product_08,
+    product_09: submission.product_09,
+    product_10: submission.product_10,
+    product_11: submission.product_11,
+    product_12: submission.product_12,
+    product_13: submission.product_13,
+    product_14: submission.product_14,
+    product_15: submission.product_15,
+    product_16: submission.product_16
+  };
+
+  const result = await requests.replaceOne(query, replacement, options);
+  
+  result();
+
+  // requests.insertOne(submission, (err, collection) => {
+  //   if(err) {
+  //     throw err;
+  //   }
+  //   console.log('Record inserted');
+  // });
 
   console.log(res.body);
 
