ef7e6da5d68a2e89f22bb0da82675f8d5b3beeed
Zach Shelton <zachshelton91@gmail.com>
2021-06-09 17:11:27 -0500

    Submitting database and server that works on localhost

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.post('/submit', async (req, res) => {
   const requests = db.collection('requests');
   const users = db.collection('users');
   // const items = db.collection('items');
+  const costArray = [];
+  let totalCost = 0;
   const availableItems = [
     {
       "productID": "product_01",
@@ -100,44 +102,67 @@ app.post('/submit', async (req, res) => {
   ];
 
   let validUser = await User.findOne({ _id: req.body.uuid });
-  let validUserObject = await JSON.parse(validUser);
-  console.log(validUserObject);
 
   if (validUser) {
+
     // Check if totalPoints fields match
     if (validUser.totalPoints == req.body.totalPoints) {
-      // Check which product fields are requested
-      // let result = Object.keys(myObj).filter(v => v.startsWith("_"));
-      let validUserProducts = Object.keys
-      console.log(validUserProducts);
+
+      // Create object with just the products from req.body
+      let submittedProducts = Object.keys(req.body)
+        .filter(key => key.indexOf('product') == 0)
+        .reduce((newData, key) => {
+          newData[key] = req.body[key];
+          return newData;
+        }, {});
+
+      // Convert object to array value pairs
+      let requestedProductsArray = Object.keys(submittedProducts)
+        .map(key => [(key), submittedProducts[key]]);
+      
+      // Do the work on products that have been requests
+      requestedProductsArray.forEach(product => {
+        if (product[1] > 0) {
+          // Create index to be matched to availableItems, remove 'product_'
+          let selectedProductID = product[0];
+          selectedProductID = selectedProductID.replace(/\D/g,'');
+
+          // Multiply submitted product value by index.value
+          let productCost = product[1] * availableItems[selectedProductID - 1].itemValue;
+
+          // Push item cost into the total cost array
+          costArray.push(productCost);
+        }
+      });
+
+      // Once the item values have been created, add them up
+      totalCost = costArray.reduce((a, b) => a + b);
+
+      // If balance is greater than zero after item validation, write to the database
+      if (req.body.totalPoints >= totalCost) {
+        try {
+          const query = { uuid: submission.uuid };
+          const update = { $set: { accountNumber: submission.accountNumber, uuid: submission.uuid, companyName: submission.companyName , firstName: submission.firstName , lastName: submission.lastName , totalPoints: submission.totalPoints , product_01: submission.product_01 , product_02: submission.product_02 , product_03: submission.product_03 , product_04: submission.product_04 , product_05: submission.product_05 , product_06: submission.product_06 , product_07: submission.product_07 , product_08: submission.product_08 , product_09: submission.product_09 , product_10: submission.product_10 , product_11: submission.product_11 , product_12: submission.product_12 , product_13: submission.product_13 , product_14: submission.product_14 , product_15: submission.product_15 , product_16: submission.product_16 }};
+          const options = { upsert: true };
+        
+          requests.updateOne(query, update, options);
+      
+          console.log('Record inserted');
+          return res.json({"status": "ok"});
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        res.status(406);
+        res.send('Your Rockets Rewards request balance exceeds your available balance. Please try again or contact your Rockets representative.');
+      }
+    } else {
+      res.status(401);
+      res.send('The Rockets Rewards balance in your request does not match our records. Please try again or contact your Rockets representative.');
     }
-  }
-
-  // This returns whole record as array
-  // items.find({}).toArray(function(err, result) {
-  //   if (err) throw err;
-  //   itemsArray = result;
-  // });
-
-  // TODO - Write a function that returns items from the submission
-  // submissionItems = await submission.find({ product_01: submission.product_01 , product_02: submission.product_02 , product_03: submission.product_03 , product_04: submission.product_04 , product_05: submission.product_05 , product_06: submission.product_06 , product_07: submission.product_07 , product_08: submission.product_08 , product_09: submission.product_09 , product_10: submission.product_10 , product_11: submission.product_11 , product_12: submission.product_12 , product_13: submission.product_13 , product_14: submission.product_14 , product_15: submission.product_15 , product_16: submission.product_16});
-
-  // TODO - Compare submission items to database items
-  // TODO - If submissions check out, call the items below to insert into db
-  // TODO - Else, send error back to client side
-
-
-  try {
-    const query = { uuid: submission.uuid };
-    const update = { $set: { accountNumber: submission.accountNumber, uuid: submission.uuid, companyName: submission.companyName , firstName: submission.firstName , lastName: submission.lastName , totalPoints: submission.totalPoints , product_01: submission.product_01 , product_02: submission.product_02 , product_03: submission.product_03 , product_04: submission.product_04 , product_05: submission.product_05 , product_06: submission.product_06 , product_07: submission.product_07 , product_08: submission.product_08 , product_09: submission.product_09 , product_10: submission.product_10 , product_11: submission.product_11 , product_12: submission.product_12 , product_13: submission.product_13 , product_14: submission.product_14 , product_15: submission.product_15 , product_16: submission.product_16 }};
-    const options = { upsert: true };
-  
-    requests.updateOne(query, update, options);
-
-    console.log('Record inserted');
-    return res.json({"status": "ok"});
-  } catch (err) {
-    console.log(err);
+  } else {
+    res.status(404);
+    res.send('User ID not found. If you believe you have received this message in error, please contact your Rockets Rewards representative.');
   }
 }); 
 
